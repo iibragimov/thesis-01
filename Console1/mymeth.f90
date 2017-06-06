f2a5f46ae03c57263ce601b864fe5692a9e25212
Ilmir Ibragimov <ilmir.soap@gmail.com>
2017-06-06 17:06:10 +0300

    Changed dz_dzeta, calculated current lines correctly

--- Console1/mymeth.f90
+++ Console1/mymeth.f90
@@ -248,6 +248,9 @@ end if
 end function
 
 !=============< Построение линий тока >==================!
+!NOTICE: 
+!       1. в моих обозначениях z -- это плоскость z, zz -- плоскость zeta
+!       2. в обозначениях Р.Ф. наоборот
 !Подключить файлы func.f90, lines_func.f90, lines_mod.f90
 !Подготовить 2 функции dz_d\zeta и dw_d\zeta
 !Так как функция dz_dzeta дает скачки, то попробуем мы используем функцию z(dw_dzeta(zeta))
@@ -275,7 +278,6 @@ complex(8) dw_dzeta, dz_dzeta, dw_dz, zz
 dw_dz = dw_dzeta(zz) / dz_dzeta(zz)
 end function 
 
-!исправить, чтобы не было скачков
 function dz_dzeta(zz)
 !нахождение функции dz_d\zeta, поынтегральная функция
 !разделил на скобки, чтоб не запупаться
@@ -283,77 +285,83 @@ function dz_dzeta(zz)
 use mod
 complex(8) zz, dz_dzeta
 complex(8) C, zz_a, zz_p, zz_b, zz_c1
-C = mod_C * cdexp(ii * arg_C)               ! комплексная константа
-zz_a = (zz - cdexp(ii * g_a))               ! (zeta - zeta_a)
-zz_c1 = (zz - c1)                           ! (zeta - c1)
-zz_p = (zz - cdexp(ii * g_p))**(d1 - delta) ! (zeta - zeta_p)
-zz_b = (zz - cdexp(ii * g_b))**(delta - d1) ! (zeta - zeta_b)^(-1)
-dz_dzeta = C * zz_a * zz_c1 * zz_p * zz_b * zz**(-d2)
+C = mod_C * cdexp(ii * arg_C)
+zz_a = (zz - cdexp(ii * g_a))
+zz_c1 = (zz - c1)
+zz_p = (zz - cdexp(ii * g_p))
+zz_b = (zz - cdexp(ii * g_b))
+dz_dzeta = (C * zz_a * zz_c1 * zz_p) * (zz_b / zz_p)**(delta) / (zz_b * zz**(d2))
+end function 
 
 !zz_p = (zz - cdexp(ii * g_p))
 !zz_b = (zz - cdexp(ii * g_b)) 
 !zz_delta = ((zz - cdexp(ii * g_b)) / (zz - cdexp(ii * g_p)))**(delta) ! ((zeta - zeta_b) / (zeta - zeta_p))**(delta)
 !dz_dzeta = C * zz_a * zz_c1 * zz_p * zz_delta / zz_b / zz**(d2)
 !
-end function 
 
 !продумать нужно 
-function lines_test_stop(z,zz)
+function lines_test_stop_my(z,zz)
 !условие остановки при построении линии тока
 !z -- что такое?
 !zz -- ?
 use mod
 complex(8) z,zz
-logical lines_test_stop
-lines_test_stop = .FALSE. !(dreal(zz) > 4.0d0) ! .OR. (cdabs(z) > 20.0d0) 
+logical lines_test_stop_my
+lines_test_stop_my = (cdabs(z) > 20.0d0) .OR. (cdabs(zz) > 20.0d0)
 end
 
-function lines_test_stop2(z,zz)
+function lines_test_stop2_my(z,zz)
 !условие остановки при построении линии тока
 !z -- ?
 !zz -- ?
 use mod
 complex(8) z,zz
-logical lines_test_stop2
-lines_test_stop2 = (dreal(zz) > 4.0d0) ! .OR. (cdabs(z) > 20.0)
+logical lines_test_stop2_my
+lines_test_stop2_my = (cdabs(z) > 20.0d0) .OR. (cdabs(zz) > 20.0d0)
 end
 
-subroutine save_line(k, array, zone_name, zone_name_n)
+subroutine save_line(current_port, k, array, zone_name, zone_name_n)
 !запись в файл линии тока по зонам
 !k -- кол-во точек линии тока
 !zl -- zl(0:nmax) комплексный массив линии тока
 !zone_name -- название зоны
 !zone_name_n -- ?
 use mod 
-integer(4) i, k, zone_name_n
+integer(4) i, k, zone_name_n, current_port
 character(8) zone_name
 complex(8) array(0:nmax)
-write(port, "('ZONE T=""', A5, i2, '"", I=', i4, ', F=POINT')") zone_name, zone_name_n, k+1
+write(current_port, "('ZONE T=""', A5, i2, '"", I=', i4, ', F=POINT')") zone_name, zone_name_n, k+1
 do i = 0, k
-    write(port, "(F9.5, ' ', F9.5)") dreal(array(i)), dimag(array(i))
+    write(current_port, "(F9.5, ' ', F9.5)") dreal(array(i)), dimag(array(i))
 enddo
 end subroutine 
 
-subroutine save_plastin
-!вспомогательная процедура для сохранения пластинки в файл, можно посути всего лишь три точки задать
-!протестить оба варианта
+subroutine save_plastin(port_z)
+!вспомогательная процедура для сохранения пластинки в файл
 use mod
-integer(4) i
+integer(4) i, port_z
 character(8) zone_name
 complex(8) z, t 
 zone_name = 'plastina'
-!первый вариант
-!write(port, "('ZONE T=""', A5, i2, '"", I=', i4, ', F=POINT')") zone_name, 34, n+1
-!do i = 1, n
-!    t = z((i - 1) * d2 * pi / (n - 1))
-!    tt(i) = t
-!    write(port, "(F9.5, ' ', F9.5)") dreal(t), dimag(t)
-!end do
-!второй вариант, 34 ая линия, 3 точек видимо
-write(port, "('ZONE T=""', A5, i2, '"", I=', i4, ', F=POINT')") zone_name, 34, 3
-write(port, "(F9.5, ' ', F9.5)") dreal(z(cdexp(ii * g_a))), dimag(z(cdexp(ii * g_a)))
-write(port, "(F9.5, ' ', F9.5)") dreal(z(cdexp(ii * g_b))), dimag(z(cdexp(ii * g_b)))
-write(port, "(F9.5, ' ', F9.5)") dreal(z(c1)), dimag(z(c1))
+write(port_z, "('ZONE T=""', A5, i2, '"", I=', i4, ', F=POINT')") zone_name, 34, 3
+write(port_z, "(F9.5, ' ', F9.5)") dreal(z(cdexp(ii * g_a))), dimag(z(cdexp(ii * g_a)))
+write(port_z, "(F9.5, ' ', F9.5)") dreal(z(cdexp(ii * g_b))), dimag(z(cdexp(ii * g_b)))
+write(port_z, "(F9.5, ' ', F9.5)") dreal(z(c1)), dimag(z(c1))
+end subroutine 
+
+subroutine save_circle(port_zz)
+!вспомогательная процедура для сохранения окружности в файл
+use mod 
+integer(4) i, port_zz, k
+character(8) zone_name 
+real(8) gj
+zone_name = 'circle'
+k = 201
+write(port_zz, "('ZONE T=""', A5, i2, '"", I=', i4, ', F=POINT')") zone_name, 35, 201
+do i = 1, k
+    gj = d2 * pi - (i - 1) * d2 * pi / (k - 1)
+    write(port_zz, "(F9.5, ' ', F9.5)") dcos(gj), dsin(gj)
+end do
 end subroutine 
 
 subroutine current_lines !(x, y, z)
@@ -362,12 +370,12 @@ subroutine current_lines !(x, y, z)
 !y -- alpha угол атаки
 !z -- l_2 длина закрылка
 use mod
-external dz_dzeta, dw_dzeta, lines_test_stop, lines_test_stop2
-logical lines_test_stop, lines_test_stop2
+external dz_dzeta, dw_dzeta, lines_test_stop_my, lines_test_stop2_my
+logical lines_test_stop_my, lines_test_stop2_my
 character(8) zone_name !название зон линий, для записи в файл
-integer(4) i, k, zone_name_n, kol_lines
+integer(4) i, k, zone_name_n, number_of_lines, port_z, port_zz
 real(8) dir0, dl, shag, dk
-complex(8) z0, zz0, zl(0:nmax), zlz(0:nmax), zz, z, dw_dzeta, dz_dzeta
+complex(8) z0, zz0, zl(0:nmax), zlz(0:nmax), z, dw_dzeta, dz_dzeta, temp_zl_k
 !Описание переменных
 !dw_dzeta -- dw_d\zeta 
 !dz_dzeta -- dz_d\zeta 
@@ -375,14 +383,15 @@ complex(8) z0, zz0, zl(0:nmax), zlz(0:nmax), zz, z, dw_dzeta, dz_dzeta
 !kol_lines -- количество линий тока
 !z0 -- начальная точка построения линии тока zeta
 !zz0 -- начальная точка построения линии тока в плосткости z
-!dir0 -- начальное направление построения линии тока в плосткости zeta
+!dir0 -- начальное направление скорости линии тока в плосткости zeta dir0 = -zarg(dw_dzeta())
 !kdir - 1 - по потоку, -1 - против потока   
-!dl -- ?
-!dk -- ?
+!upper_bound -- верхняя граница линий тока в плоскости zeta
+!lower_bound -- нижняя граница линий тока в плоскости zeta
 !zl - выходной массив точек в плоскости zeta
 !zlz - выходной массив точек в плоскости z
-!zone_name_k -- ?
-!zz -- ?
+!zone_name_n -- индекс линий тока, (чтоб в файле не путать)
+!z_port -- индекс для записи в файл линий тока в плоскости z
+!zz_port -- индекс для записи в файл линий тока в плоскости zeta 
 
 !нахождение параметров/констант задачи
 !call find_const !возможно придется убрать и искать вне этой процедуры,тогда не нужно будет передавать xyz
@@ -396,63 +405,65 @@ call init_lines_const
 !Потом вызвать процедуру в противоположную сторону интегрирования
 !нужно использовать find_lines2
 !написать lines_test_stop универсальный
-!отыскание 
+
+port_z = port
+port_zz = port + 1
+open(port_z, FILE='data/current_lines_z.dat')
+open(port_zz, FILE='data/current_lines_zeta.dat')
 
 !Предполагаемо нашли точку разветвления потока
 z0 = cdexp(ii * g_o)
 zz0 = z(z0)
 dir0 = g_o
-call find_line2(z0, zz0, dir0, -1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop)
+call find_line2(z0, zz0, dir0, -1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop_my)
 zone_name = 'first'
-open(port, FILE='data/current_lines.dat')
 !do i = 0, k
 !    zl(i) = z(zl(i))
 !end do
-call save_line(k, zlz, zone_name, 0)
+call save_line(port_z, k, zlz, zone_name, 0)
+call save_line(port_zz, k, zl, zone_name, 0)
 
-!kol = 31
-!dl = dimag(zlz(k)) * d2 *5   ! dl - переименовать, чтоб было понятно
-!dk = dreal(zlz(k))           ! dk - переименовать, и найти zlz(k),т.к. нужно на что-то опираться.
-!shag = dl / (kol - 1)
-!do zone_name_n = 1, kol
-!    zz0 = dk * c1 + ii * shag * zone_name_n
-!    z0 = z(zz0)
-!    dir0 = -zarg(dw_dzeta(zz0)) !?? zarg()
-!    call find_line2(z0, zz0, dir0, 1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop2)
-!    zone_name = char(47 + zone_name_n)
-!    call save_line(k, zlz, zone_name, zone_name_n)
-!end do
+temp_zl_k = zl(k)
+number_of_lines = 20
+shag = 0.02d0
+!upper_bound = dimag(zl(k)) * d2 *5   
+!lower_bound = dreal(zl(k))
 
-z0 = zl(k) - ii * 0.02d0 !в плоскости zeta
-zz0 = z(z0)             !в плоскости z
-dir0 = -zarg(dw_dzeta(z0))
-call find_line2(z0, zz0, dir0, 1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop2)
-zone_name = 'down' !char(47 + zone_name_n)
-call save_line(k, zlz, zone_name, zone_name_n)
-
-z0 = zl(0) + ii * 0.04d0 !в плоскости zeta
-zz0 = z(z0)             !в плоскости z
-dir0 = -zarg(dw_dzeta(z0))
-call find_line2(z0, zz0, dir0, 1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop2)
-zone_name = 'down' !char(47 + zone_name_n)
-call save_line(k, zlz, zone_name, zone_name_n)
-
-z0 = zl(k) + ii * 1.0d0 !в плоскости zeta
-zz0 = z(z0)             !в плоскости z
-dir0 = -zarg(dw_dzeta(z0))
-call find_line2(z0, zz0, dir0, 1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop2)
-zone_name = 'up' !char(47 + zone_name_n)
-call save_line(k, zlz, zone_name, zone_name_n)
+!над разветлвяющейся линией
+do zone_name_n = 1, number_of_lines + 5
+    z0 = temp_zl_k * cdexp(-ii * shag * zone_name_n)    ! в плоскости zeta
+    zz0 = z(z0)                                         ! в плоскости z
+    dir0 = -zarg(dw_dzeta(z0))
+    call find_line2(z0, zz0, dir0, 1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop2_my)
+    zone_name = char(47 + zone_name_n)
+    call save_line(port_z, k, zlz, zone_name, zone_name_n)
+    call save_line(port_zz, k, zl, zone_name, zone_name_n)    
+end do
+!под разветлвяющейся линией
+do zone_name_n = 1, number_of_lines
+    z0 = temp_zl_k * cdexp(ii * shag * zone_name_n)    ! в плоскости zeta
+    zz0 = z(z0)                                         ! в плоскости z
+    dir0 = -zarg(dw_dzeta(z0))
+    call find_line2(z0, zz0, dir0, 1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop2_my)
+    zone_name = char(47 + zone_name_n)
+    call save_line(port_z, k, zlz, zone_name, zone_name_n)
+    call save_line(port_zz, k, zl, zone_name, zone_name_n)    
+end do
 
 !этот кусок рисует линию тока, которая от пластинки отходит
 !нужно его продумать, а еще добавить выдув струи по хорошему для второй задачи, а не для первой
 z0 = c1
 zz0 = z(c1) !вроде задняя кромка закрылка 
 dir0 = -zarg(dw_dzeta(z0)) !может стоит zarg(z0) взять, поток вроде как по направления закрылка стекает, понять нужно почему именно -zarg(dw_dzeta(z0))
-call find_line2(z0, zz0, dir0, 1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop2)
+call find_line2(z0, zz0, dir0, 1, zl, zlz, k, nmax, dw_dzeta, dz_dzeta, lines_test_stop2_my)
 zone_name = 'line' !char(47 + zone_name_n + 1)
-call save_line(k, zlz, zone_name, 0) !zone_name_n)
-call save_plastin
-close(port)
-port = port + 1
+call save_line(port_z, k, zlz, zone_name, 0)
+call save_line(port_zz, k, zl, zone_name, 0)
+
+call save_plastin(port_z)
+call save_circle(port_zz)
+
+close(port_z)
+close(port_zz)
+port = port + 2
 end subroutine 
